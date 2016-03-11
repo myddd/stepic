8992026578eb258a87bc9d2f8e59a486acdd6f51
myddd <myddd@tut.by>
2016-03-11 16:04:02 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def application (env, start_response):
 	qs = parse_qs (env['QUERY_STRING'])
 	res = ''
 	for k,v in qs.iteritems():
-		res += k + '=' + v + '\n'
+		res += str(k) + '=' + str(v) + '\n'
 	start_response ('200 OK', [('Content-Type', 'text/plain'),('Content-Length',str(len(res)))])
 	return [res]
