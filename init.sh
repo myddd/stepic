918b33d7d95137cf9e8f48d81bcaf378626f8413
myddd <myddd@tut.by>
2016-03-11 16:38:43 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 sudo ﻿ln -s /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello.application
