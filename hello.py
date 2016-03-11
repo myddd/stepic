339efe2d062060f7675ce9161cd8389061653889
myddd <myddd@tut.by>
2016-03-11 15:37:59 +0300

    1

---
+++ hello.py
@@ -0,0 +1,9 @@
+from cgi import parse_qs
+
+def application (env, start_response):
+	qs = parse_qs (env['QUERY_STRING'])
+	res = ''
+	for k,v in qs.iteritems():
+		res += k + '=' + v + '\n'
+	start_response ('200 OK', [('Content-Type', 'text/plain'),('Content-Length',str(len(res)))])
+	return [res]
