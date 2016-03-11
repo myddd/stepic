6b6fad6297261a20245011703d841dfcb86d1394
myddd <myddd@tut.by>
2016-03-11 16:25:36 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def application (env, start_response):
 	print env['QUERY_STRING']
 	resp = env['QUERY_STRING'].split("&")
   	resp = [item+"\r\n" for item in resp]
-  	start_response ('200 OK', [('Content-Type', 'text/plain'),('Content-Length',str(len(resp)))])
+  	start_response ('200 OK', [('Content-Type', 'text/plain')])
   	return resp
 	
