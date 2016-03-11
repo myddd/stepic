fc179cb13f055fc4a4b609c80cb041d1d67dbd69
myddd <myddd@tut.by>
2016-03-11 16:22:44 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 from cgi import parse_qs
 
 def application (env, start_response):
+	print env['QUERY_STRING']
 	resp = env['QUERY_STRING'].split("&")
   	resp = [item+"\r\n" for item in resp]
   	start_response ('200 OK', [('Content-Type', 'text/plain'),('Content-Length',str(len(resp)))])
